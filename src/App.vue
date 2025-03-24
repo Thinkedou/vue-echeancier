687bab93e31e768e344063540949eb7bc3c38224
Thinkedou <r.enguehard@gmail.com>
2025-03-24 11:40:49 +0100

    feat: first version directly in v-for directive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,84 @@
-<script setup></script>
+<script setup>
+// ICI JE SUIS EN JS 
+const emprunt = 2000
+const dureeEnMois = 10
+const mensualite = emprunt/dureeEnMois
+
+console.log('à rembourser chaque mois' , mensualite)
+
+const debut = new Date()
+console.log(debut)
+
+
+
+
+
+
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
+   <table>
+  <caption>
+    Emprunt: {{ emprunt }}€, sur {{ dureeEnMois }} mois
+  </caption>
+  <thead>
+    <tr>
+      <th scope="col">Mois</th>
+      <th scope="col">à rembourser</th>
+      <th scope="col">Reste du</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr v-for="monthly in dureeEnMois">
+      <th scope="row">{{new Date(debut.setMonth(debut.getMonth()+1)).toISOString()}}</th>
+      <td>{{mensualite}}</td>
+      <td>{{ emprunt-(mensualite*monthly) }}</td>
+    </tr>
+  </tbody>
+</table>
 </template>
 
-<style scoped></style>
+<style scoped>
+table {
+  border-collapse: collapse;
+  border: 2px solid rgb(140 140 140);
+  font-family: sans-serif;
+  font-size: 0.8rem;
+  letter-spacing: 1px;
+}
+
+caption {
+  padding: 10px;
+  font-weight: bold;
+}
+
+thead,
+tfoot {
+  background-color: rgb(228 240 245);
+}
+
+th,
+td {
+  border: 1px solid rgb(160 160 160);
+  padding: 8px 10px;
+}
+
+td:last-of-type {
+  text-align: center;
+}
+
+tbody > tr:nth-of-type(even) {
+  background-color: rgb(237 238 242);
+}
+
+tfoot th {
+  text-align: right;
+}
+
+tfoot td {
+  font-weight: bold;
+}
+
+
+</style>
